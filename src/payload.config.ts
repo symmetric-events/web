6c73c05ab0650b0cb1d95cf17add99830bca9151
Matúš Laco <laco.matus@gmail.com>
2025-12-19 12:23:18 +0100

    Update payload.config.ts

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -1,21 +1,20 @@
-import { vercelBlobStorage } from '@payloadcms/storage-vercel-blob'
 import { postgresAdapter } from '@payloadcms/db-postgres'
 import { lexicalEditor } from '@payloadcms/richtext-lexical'
 import path from 'path'
 import { buildConfig } from 'payload'
 import { fileURLToPath } from 'url'
 import sharp from 'sharp'
-import { env } from './env'
 
 import { Users } from './collections/Users'
 import { Media } from './collections/Media'
+import { vercelBlobStorage } from '@payloadcms/storage-vercel-blob'
 import { Events } from './collections/Events'
 import { Trainers } from './collections/Trainers'
-import { Testimonials } from './collections/Testimonials'
 import { Categories } from './collections/Categories'
+import { Testimonials } from './collections/Testimonials'
 import { Orders } from './collections/Orders'
-import { DiscountCodes } from './collections/DiscountCodes'
 import { Blog } from './collections/Blog'
+import { DiscountCodes } from './collections/DiscountCodes'
 
 const filename = fileURLToPath(import.meta.url)
 const dirname = path.dirname(filename)
@@ -28,26 +27,25 @@ export default buildConfig({
     },
   },
   collections: [Users, Media, Events, Trainers, Testimonials, Categories, Orders, DiscountCodes, Blog],
+
   editor: lexicalEditor(),
-  secret: env.PAYLOAD_SECRET,
+  secret: process.env.PAYLOAD_SECRET || '',
   typescript: {
     outputFile: path.resolve(dirname, 'payload-types.ts'),
   },
   db: postgresAdapter({
     pool: {
-      connectionString: env.DATABASE_URL,
+      connectionString: process.env.DATABASE_URI || '',
     },
-    migrationDir: path.resolve(dirname, 'migrations'),
   }),
   sharp,
   plugins: [
     vercelBlobStorage({
-      enabled: true,
       collections: {
         media: true,
       },
-      token: env.BLOB_READ_WRITE_TOKEN!,
-      clientUploads: true,
+      // Token provided by Vercel once Blob storage is added to your Vercel project
+      token: process.env.BLOB_READ_WRITE_TOKEN,
     }),
   ],
 })
